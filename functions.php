ae6461211a1d852069f1bd37a118aed4442a18be
Valerio Bozzolan <boz@reyboz.it>
2017-02-16 22:51:03 +0100

    Moved enfatize_substr() to it's own class

--- functions.php
+++ functions.php
@@ -107,36 +107,9 @@ function force_array( & $a ) {
  * @param $pre string HTML before query (bold tag as default)
  * @param $post string HTML after query (bold tag as default)
  * @return string Enfatized string
- * @todo Move in it's own class
  */
-function enfatize_substr($s, $q, $pre = "<b>", $post = "</b>") {
-	$s_length = strlen($s);
-	$q_length = strlen($q);
-
-	$offset = 0;
-	do {
-		// Find position
-		$pos = stripos($s, $q, $offset);
-		if($pos === false) {
-			break;
-		}
-
-		// Enfatize query
-		$enfatized = $pre . substr($s, $pos, $q_length) . $post;
-		$enfatized_length = strlen($enfatized);
-
-		// Pre-query and post-query strings
-		$s_pre = substr($s, 0, $pos);
-		$s_post = substr($s, $pos + $q_length);
-
-		// Save
-		$s = $s_pre . $enfatized . $s_post;
-
-		$offset = $pos + $enfatized_length;
-
-	} while($offset < $s_length);
-
-	return $s;
+function enfatize_substr($s, $q, $pre = '<b>', $post = '</b>') {
+	return EnfatizeSubstr::get($s, $q, $pre, $post);
 }
 
 /*
@@ -585,9 +558,11 @@ function URL_root() {
  * HTTP 503 header
  */
 function http_503() {
-	header('HTTP/1.1 503 Service Temporarily Unavailable');
-	header('Status: 503 Service Temporarily Unavailable');
-	header('Retry-After: 300');
+	if( ! headers_sent() ) {
+		header('HTTP/1.1 503 Service Temporarily Unavailable');
+		header('Status: 503 Service Temporarily Unavailable');
+		header('Retry-After: 300');
+	}
 }
 
 /**
@@ -618,6 +593,7 @@ function error_die($msg) {
 <head>
 	<title><?php _e("Errore") ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET ?>" />
+	<meta name="robots" content="noindex, nofollow" />
 </head>
 <body>
 	<h1><?php printf(
