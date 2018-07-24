336092467a9b2a9a0772aa0fb1a27b16b28e6ec0
Valerio Bozzolan <boz@reyboz.it>
2018-07-24 16:05:14 +0200

    using 'or' instead of '||' for non comparisons

--- load.php
+++ load.php
@@ -1,5 +1,5 @@
 <?php
-# Copyright (C) 2015 Valerio Bozzolan
+# Copyright (C) 2015, 2018 Valerio Bozzolan
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,10 +19,10 @@
  */
 
 // URL slash
-defined('_')  || define('_', '/');
+defined('_')  or define('_', '/');
 
 // Directory separator
-defined('__') || define('__', DIRECTORY_SEPARATOR);
+defined('__') or define('__', DIRECTORY_SEPARATOR);
 
 define('BOZ_PHP', __DIR__ ); // Here
 require BOZ_PHP . __ . 'functions.php'; // Some functions
@@ -31,19 +31,19 @@ require BOZ_PHP . __ . 'functions.php'; // Some functions
 get_page_load();
 
 // Lot of constants with default values
-defined('DEBUG')                    || define('DEBUG', false);
-defined('PROTOCOL')                 || define('PROTOCOL', URL_protocol() );
-defined('DOMAIN')                   || define('DOMAIN', URL_domain() );
-defined('PORT')                     || define('PORT', URL_port() );
-defined('ROOT')                     || define('ROOT', URL_root() );
-defined('URL')                      || define('URL', PROTOCOL . DOMAIN . PORT . ROOT );
-defined('SHOW_EVERY_SQL')           || define('SHOW_EVERY_SQL', false);
-defined('REQUIRE_LOAD_POST')        || define('REQUIRE_LOAD_POST', true);
-defined('CHMOD_WRITABLE_DIRECTORY') || define('CHMOD_WRITABLE_DIRECTORY', 0777);
-defined('CHMOD_WRITABLE_FILE')      || define('CHMOD_WRITABLE_FILE', 0666);
-defined('MAGIC_MIME_FILE')          || define('MAGIC_MIME_FILE', null); // Fifo system default
-defined('CHARSET')                  || define('CHARSET', 'utf-8');
-defined('CACHE_BUSTER')             || define('CACHE_BUSTER', '');
+defined('DEBUG')                    or define('DEBUG', false);
+defined('PROTOCOL')                 or define('PROTOCOL', URL_protocol() );
+defined('DOMAIN')                   or define('DOMAIN', URL_domain() );
+defined('PORT')                     or define('PORT', URL_port() );
+defined('ROOT')                     or define('ROOT', URL_root() );
+defined('URL')                      or define('URL', PROTOCOL . DOMAIN . PORT . ROOT );
+defined('SHOW_EVERY_SQL')           or define('SHOW_EVERY_SQL', false);
+defined('REQUIRE_LOAD_POST')        or define('REQUIRE_LOAD_POST', true);
+defined('CHMOD_WRITABLE_DIRECTORY') or define('CHMOD_WRITABLE_DIRECTORY', 0777);
+defined('CHMOD_WRITABLE_FILE')      or define('CHMOD_WRITABLE_FILE', 0666);
+defined('MAGIC_MIME_FILE')          or define('MAGIC_MIME_FILE', null); // Fifo system default
+defined('CHARSET')                  or define('CHARSET', 'utf-8');
+defined('CACHE_BUSTER')             or define('CACHE_BUSTER', '');
 
 // On demand requests class-$php ... it's f****** amazing!
 spl_autoload_register( function($c) {
@@ -67,7 +67,7 @@ $GLOBALS['G']->add('registerLanguage', 'RegisterLanguage');
 
 // Callback
 if( REQUIRE_LOAD_POST ) {
-	defined('ABSPATH') || error_die( __("Devi definire la costante ABSPATH. Oppure disabilita REQUIRE_LOAD_POST.") );
+	defined('ABSPATH') or error_die( __("Devi definire la costante ABSPATH. Oppure disabilita REQUIRE_LOAD_POST.") );
 
 	require ABSPATH . __ . 'load-post.php';
 }
