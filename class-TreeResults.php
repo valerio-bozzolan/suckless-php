ab88680b657accbeec26245187aa43303fa28fe0
Valerio Bozzolan <boz@reyboz.it>
2019-01-09 14:02:03 +0100

    deprecate TreeResults

--- class-TreeResults.php
+++ class-TreeResults.php
@@ -14,6 +14,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * TODO: kill this unuseful class
+ *
+ * @deprecated
+ */
 class TreeResults {
 	/**
 	 * @type DynamicQuery
