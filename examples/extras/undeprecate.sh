5ab62d3b3e6945e48e861ae1e25db99af5ed317b
Valerio Bozzolan <root@debianlander>
2019-06-27 14:05:00 +0200

    improve undeprecate.sh

--- examples/extras/undeprecate.sh
+++ examples/extras/undeprecate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sed -i 's/_esc_html(/echo esc_html(/' *.{html,php}
-sed -i 's/_esc_attr(/echo esc_attr(/' *.{html,php}
-sed -i 's/_value(/echo value(/'       *.{html,php}
-sed -i 's/_selected(/echo selected(/' *.{html,php}
-sed -i 's/_checked(/echo checked(/'   *.{html,php}
-sed -i 's/<?php echo/<?=/'            *.{html,php}
+sed -i 's/ _esc_html(/ echo esc_html(/' $@
+sed -i 's/ _esc_attr(/ echo esc_attr(/' $@
+sed -i 's/ _value(/ echo value(/'       $@
+sed -i 's/ _selected(/ echo selected(/' $@
+sed -i 's/ _checked(/ echo checked(/'   $@
+sed -i 's/ _e(/ echo __(/'              $@
+sed -i 's/<?php echo/<?=/'              $@
