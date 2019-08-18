9665b0cd574c1f5be6cc20ae05cf354353676cc1
Valerio Bozzolan <boz@reyboz.it>
2019-08-18 20:37:36 +0200

    improve undeprecate script

--- examples/extras/undeprecate.sh
+++ examples/extras/undeprecate.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-sed -i 's/ _esc_html(/ echo esc_html(/' $@
-sed -i 's/ _esc_attr(/ echo esc_attr(/' $@
-sed -i 's/ _value(/ echo value(/'       $@
-sed -i 's/ _selected(/ echo selected(/' $@
-sed -i 's/ _checked(/ echo checked(/'   $@
-sed -i 's/ _e(/ echo __(/'              $@
-sed -i 's/<?php echo/<?=/'              $@
+set -e
+sed -i 's/ _esc_html(/ echo esc_html(/g' $@
+sed -i 's/ _esc_attr(/ echo esc_attr(/g' $@
+sed -i 's/ _value(/ echo value(/g'       $@
+sed -i 's/ _selected(/ echo selected(/g' $@
+sed -i 's/ _checked(/ echo checked(/g'   $@
+sed -i 's/ _e(/ echo __(/g'              $@
+sed -i 's/ and echo/ and print/g'        $@
+sed -i 's/<?php echo/<?=/g'              $@
+sed -i 's/<?= echo/ <?=/g'               $@
