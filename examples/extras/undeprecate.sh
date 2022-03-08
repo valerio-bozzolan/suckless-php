0c239ad14f630f8e04bc8355c9d487970edafc8d
Valerio Bozzolan <boz@reyboz.it>
2022-03-08 19:54:34 +0100

    undeprecate.sh: convert _() to __()

--- examples/extras/undeprecate.sh
+++ examples/extras/undeprecate.sh
@@ -6,6 +6,7 @@ sed -i 's/ _value(/ echo value(/g'        $@
 sed -i 's/ _selected(/ echo selected(/g'  $@
 sed -i 's/ _checked(/ echo checked(/g'    $@
 sed -i 's/ _e(/ echo __(/g'               $@
+sed -i 's/ _(/ __(/g'                     $@
 sed -i 's/ and echo/ and print/g'         $@
 sed -i 's/<?php echo/<?=/g'               $@
 sed -i 's/<?= echo/ <?=/g'                $@
